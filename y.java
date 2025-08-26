9dd70c992f1d787d6ed1d6a749661efa710d6182
Mohamed <159513882+Mohaxq@users.noreply.github.com>
2025-08-26 18:46:53 +0300

    Update y.java

--- y.java
+++ y.java
@@ -1,5 +1,5 @@
 public class y {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, Eng. Nada Adel. We want bonus please.");
     }
 }
